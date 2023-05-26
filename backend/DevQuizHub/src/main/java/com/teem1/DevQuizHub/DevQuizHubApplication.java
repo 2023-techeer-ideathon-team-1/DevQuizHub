d89887e15cba90b5c9312b3ebe29e95dd48806bb
baejunil <bjo6300@naver.com>
2023-05-27 01:33:22 +0900

    chore : 프로젝트 초기 세팅 & .gitignore 추가

---
+++ backend/DevQuizHub/src/main/java/com/teem1/DevQuizHub/DevQuizHubApplication.java
@@ -0,0 +1,13 @@
+package com.teem1.DevQuizHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevQuizHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevQuizHubApplication.class, args);
+	}
+
+}
---
+++ backend/DevQuizHub/src/test/java/com/teem1/DevQuizHub/DevQuizHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teem1.DevQuizHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevQuizHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
